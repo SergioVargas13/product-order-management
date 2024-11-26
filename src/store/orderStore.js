543bd2e7617f1ac86bf6cabee4f96371811d97ed
SergioVargas13 <58775720+SergioVargas13@users.noreply.github.com>
2024-11-26 17:09:07 -0500

    Add file with crud logic for products and purchase orders
    Add file with crud logic for products and purchase orders

---
+++ src/store/orderStore.js
@@ -0,0 +1,70 @@
+import { ref } from "vue";
+import { generateNewId } from '../config/globals';
+
+export function orderStore() {
+  const orders = ref([]);
+
+  const fetchOrders = async () => {
+    try {
+      const response = await fetch("/orders.json");
+      
+      if (!response.ok) {
+        console.error("Failed to fetch orders: ", response.statusText);
+        return;
+      }
+
+      orders.value = await response.json();
+      console.log("Fetched orders: ", orders.value);
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+    }
+  };
+
+  const modifyOrderList = (action, order) => {
+    let orderIndex;
+    switch (action) {
+      case "add":
+        orders.value.push({ ...order, id: generateNewId(orders) });
+        console.log("Add order: ", order);
+        break;
+      case "update":
+        orderIndex = orders.value.findIndex((o) => o.id === order.id);
+        
+        if (orderIndex === -1) {
+          console.error("Order not found for update: ", order);
+          return;
+        }
+
+        orders.value[orderIndex] = { ...order };
+        console.log("Updated order: ", order);
+
+        break;
+      case "delete":
+        orderIndex = orders.value.findIndex((p) => p.id === order.id);
+
+        if (orderIndex === -1) {
+          console.error("Deleted order: ", deleteOrder);
+          return;
+        }
+
+        const [deleteOrder] = orders.value.splice(orderIndex, 1);
+        console.log("Deleted order: ", deleteOrder);
+
+        break;
+      default:
+        console.error("Unknown action:", action);
+    }
+  };
+
+  const addOrder = async (order) => modifyOrderList("add", order);
+  const updateOrder = async (order) => modifyOrderList("update", order);
+  const deleteOrder = async (order) => modifyOrderList("delete", order);
+
+  return {
+    orders,
+    fetchOrders,
+    addOrder,
+    updateOrder,
+    deleteOrder,
+  };
+}
---
+++ src/store/productStore.js
@@ -0,0 +1,70 @@
+import { ref } from "vue";
+import { generateNewId } from '../config/globals';
+
+export function productStore() {
+  const products = ref([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("/products.json");
+
+      if (!response.ok) {
+        console.error("Failed to fetch products: ", response.statusText);
+        return;
+      }
+
+      products.value = await response.json();
+      console.log("Fetched products:", products.value);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  const modifyProductList = (action, product) => {
+    let productIndex;
+    switch (action) {
+      case "add":
+        products.value.push({ ...product, id: generateNewId(products) });
+        console.log("Add product:", product);
+        break;
+      case "update":
+        productIndex = products.value.findIndex((p) => p.id === product.id);
+        
+        if (productIndex === -1) {
+          console.error("Product not found for update: ", product);
+          return;
+        }
+
+        products.value[productIndex] = { ...product };
+        console.log("Update product: ", product);
+
+        break;
+      case "delete":
+        productIndex = products.value.findIndex((p) => p.id === product.id);
+
+        if (productIndex === -1) {
+          console.error("Deleted product: ", deleteProduct);
+          return;
+        }
+
+        const [deletedProduct] = products.value.splice(productIndex, 1);
+        console.log("Deleted product: ", deleteProduct);
+
+        break;
+      default:
+        console.error("Unknown action:", action);
+    }
+  };
+
+  const addProduct = async (product) => modifyProductList("add", product);
+  const updateProduct = async (product) => modifyProductList("update", product);
+  const deleteProduct = async (order) => modifyProductList("delete", order);
+
+  return {
+    products,
+    fetchProducts,
+    addProduct,
+    updateProduct,
+    deleteProduct,
+  };
+}
